505c672835bf393547f4b25bd10cdb345af5863a
corey <corey@system76-pc.localdomain>
2020-03-10 09:27:27 -0500

    edited example code

--- examples/gymTesting.py
+++ examples/gymTesting.py
@@ -2,11 +2,6 @@ import gym
 env = gym.make('LunarLander-v2')
 env.reset()
 
-OBSERVATION_INDEX = 0
-REWARD_INDEX = 1
-DONE_INDEX = 2
-INFO_INDEX = 3
-
 for _ in range(30):
     env.render()
     observation, reward, done, info = env.step(env.action_space.sample())
