fe4222d4f4555b3449b30ae629ac49eeaddafde4
corey <corey@system76-pc.localdomain>
2020-07-09 20:43:46 -0500

    added new examples and restructured old ones to be more clear

--- examples/frameByFrameTesting.py
+++ examples/frameByFrameTesting.py
@@ -2,16 +2,19 @@ import retro
 
 def main(game= 'StreetFighterIISpecialChampionEdition-Genesis',  state= "chunli"):
     env = retro.make(game= game, state= state)
-    obs = env.reset()
+    env.reset()
     while True:
         env.render()
         userInput = input("Enter a set of moves in the action space {0}:".format(env.action_space))
-        separateInputs = userInput.split(',')
-        action = [int(value) for value in separateInputs]
-        obs, rew, done, info = env.step(action)
+        try:
+            separateInputs = userInput.split(',')
+            action = [int(value) for value in separateInputs]
+        except:
+            action = [0] * len(env.action_space.sample())
+        _, _, done, _ = env.step(action)
         if done:
-            obs = env.reset()
+            env.reset()
     env.close()
 
-if __name__ == "__main__":
+if __name__ == "__main__": 
     main()
